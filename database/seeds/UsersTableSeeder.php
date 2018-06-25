36d289b4c3dc52443976c2ddd08f43ee071c58ed
gudangcoding <40495749+gudangcoding@users.noreply.github.com>
2018-06-25 17:53:01 +0700

    APlikasi Pos Laravel V.01 250618

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('users')->insert(array(
+        [
+         'name' => 'Daffa\' Shidqi', 
+         'email' => 'daffa@gmail.com',
+         'password' => bcrypt('daffashidqi'),
+         'foto' => 'user.png',
+         'level' => 1
+        ],
+        [
+         'name' => 'Annisa Nabil', 
+         'email' => 'nabil@gmail.com',
+         'password' => bcrypt('annisanabil'),
+         'foto' => 'user.png',
+         'level' => 2
+        ]
+      ));
+
+    }
+}
